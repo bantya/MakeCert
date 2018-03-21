4e65c02b0e88e89883ccdf05b9ec489d73218369
Rahul Thakare <rahulthakare1504@gmail.com>
2018-03-21 22:52:18 +0530

    Added config, help, Bumped vesion to 2.0.0

--- README.md
+++ README.md
@@ -11,6 +11,15 @@ Using this file (makecert.bat) you can create a custom self-signed ssl certifica
 3. Info preview before actual ssl certs generation.
 4. If you dont approve the previewed info to be used, you can enter new info again.
 5. Confirmation to generate your ssl certs.
+6. Provide secondary fields (Country, State, Email etc.) in a single config file. **(v2.0.0)**
+
+## 📃 Changelog:
+**v2.0.0**:
+
+1. Added support to set the Country, State, City, Organization, Organization Unit, Email in a config file.
+2. Added support to use the config file for providing the above mentioned fields.
+3. Command help added.
+4. Functionality for generating `.key` and `.cert` files without `.cnf` file added.
 
 ## 🤔 How to use:
 Download the [makecert.bat](https://raw.githubusercontent.com/bantya/MakeCert/master/makecert.bat) file to your desired folder and [add it to the system  path](https://www.howtogeek.com/118594/how-to-edit-your-system-path-for-easy-command-line-access/).
@@ -19,9 +28,36 @@ Sign out if required.
 
 Use it from anywhere!
 
+## ⚙ Options:
+**--no-cfg** or **-n**: Use this option if you dont want `.cnf` file to be outputted.
+
+**--set-config** or **-s**: Use this option to populate the config fields interactively.
+The config file is stored at `%USERPROFILE%\makecert-config.ini`
+
+The makecert-config.ini example:
+```ini
+COUNTRY=IN
+STATE=MH
+CITY=Mumbai
+ORGANIZATION=example.com
+ORGANIZATION_UNIT=Backend
+EMAIL=administrator@example.com
+```
+The given config file does not exist out of the box. You have to generate it using this option.
+
+**--use-config** or **-c**: Using this option you will have to provide the hostname and domain extension only. All the other data is grabbed from the config file.
+
+**--open-repo** or **-o**: Open the project github repository in your browser.
+
+**--help** or **-h**: Display the help regarding the options used.
+
 ## 📷 Screenshots:
 ![image](https://user-images.githubusercontent.com/16685565/35646358-f918b94a-06f4-11e8-8895-b4644edbf9b9.png)
+
 ![image](https://user-images.githubusercontent.com/16685565/35646371-08a947bc-06f5-11e8-85a9-e54b3c53f55c.png)
+
+![image](https://user-images.githubusercontent.com/16685565/37725806-d0829f06-2d59-11e8-9fd8-edb744a21c8d.png)
+
 
 ### NOTE: The info to be provided must not contain any blank-space ( ), use hyphen (-) or underscore (_) instead!
 ### NOTE: This project assumes that the [openssl](https://github.com/openssl/openssl) is available on your system.
--- makecert.bat
+++ makecert.bat
@@ -5,9 +5,46 @@
 :: Purpose :    Create self-signed ssl certificates
 :: Based on :   https://gist.github.com/vikas5914/f4384d12866fd572bfc5bcf389e756e8#file-makecert-bat
 
-set VERSION=1.0.0
+set VERSION=2.0.0
+set OPTION=%1
+set SECOND_OPTION=%2
+
+set CONFIG_FILE=%USERPROFILE%\makecert-config.ini
+set CONFIG_TEMP=%TEMP%\makecert-config.txt
+
+set HOSTNAME=""
+set DOT=""
+set COUNTRY=""
+set STATE=""
+set CITY=""
+set ORGANIZATION=""
+set ORGANIZATION_UNIT=""
+set EMAIL=""
 
 title OpenSSL Cert Creator v%VERSION%
+
+if "%OPTION%" equ "--help" (
+    goto ShowHelp
+)
+if "%OPTION%" equ "-h" (
+    goto ShowHelp
+)
+
+if "%OPTION%" equ "--set-config" (
+    goto SetConfig
+)
+if "%OPTION%" equ "-s" (
+    goto SetConfig
+)
+
+if "%OPTION%" equ "--open-repo" (
+    start https://github.com/bantya/MakeCert
+    goto :EOF
+)
+if "%OPTION%" equ "-o" (
+    start https://github.com/bantya/MakeCert
+    goto :EOF
+)
 
 echo.
 echo Welcome to OpenSSL Cert Creator v%VERSION% by www.github.com/bantya
@@ -26,14 +63,21 @@ if %HOSTNAME%=='' (
 )
 
 set DOT=''
-echo What's your domain extension? (.com)
+echo What's your domain extension? (.test)
 set /p DOT=
 if %DOT%=='' (
-    set DOT=com
+    set DOT=test
 )
 
 if %DOT:~0,1%==. (
     set DOT=%DOT:~1%
+)
+
+if "%OPTION%" equ "--use-config" (
+    goto GetConfig
+)
+if "%OPTION%" equ "-c" (
+    goto GetConfig
 )
 
 set COUNTRY=''
@@ -78,6 +122,8 @@ if %EMAIL%=='' (
     set EMAIL=administrator@%HOSTNAME%.%DOT%
 )
 
+:DisplayInfo
+
 echo.
 echo Hostname : %HOSTNAME%
 echo Dot : %DOT%
@@ -89,20 +135,21 @@ echo Organization_unit : %ORGANIZATION_UNIT%
 echo Email : %EMAIL%
 
 echo.
-echo Does this info looks good to you?
+echo Does this looks good to you?
 echo If yes press y to proceed else press n to fill the info again!
+echo.
 
-SET /P CHOICE=Would you like to continue? (y/n):
-if /i %CHOICE%==n (
+SET /P CHOICE=Would you like to continue? (y/n) :
+if /i "%CHOICE%"=="n" (
     cls
     goto EnterInfo
+) else if /i "%CHOICE%"=="y" (
+    goto ProceedAhead
 )
-if /i %CHOICE%==y goto ProceedAhead
 
 :ProceedAhead
 
 echo.
-
 (
 echo [req]
 echo default_bits = 2048
@@ -130,6 +177,13 @@ echo DNS.2 = %HOSTNAME%.%DOT%
 
 call openssl req -new -x509 -newkey rsa:2048 -sha256 -nodes -keyout %HOSTNAME%.%DOT%.key -days 3560 -out %HOSTNAME%.%DOT%.crt -config %HOSTNAME%.%DOT%.cnf
 
+if "%OPTION%" equ "--no-cfg" (
+    del /q %HOSTNAME%.%DOT%.cnf
+)
+if "%OPTION%" equ "-n" (
+    del /q %HOSTNAME%.%DOT%.cnf
+)
+
 if errorlevel 1 (
     goto ErrorMessage
 )
@@ -139,12 +193,109 @@ echo Generated your %HOSTNAME%.%DOT%.key %HOSTNAME%.%DOT%.cnf %HOSTNAME%.%DOT%.c
 goto OpenFolder
 
 :ErrorMessage
+
 echo.
 echo Some error occured.
 echo Could not generate some files.
 goto OpenFolder
 
 :OpenFolder
+
 echo Opening containing folder..
 start .
 pause
+goto :EOF
+
+:GetConfig
+
+for /f "tokens=1,2 delims==" %%a in (%CONFIG_FILE%) do (
+    set %%a=%%b
+)
+goto DisplayInfo
+
+:SetConfig
+
+echo.Set your config fields here.
+echo.
+set COUNTRY=IN
+echo What's your country? (IN)
+set /p COUNTRY=
+echo COUNTRY=%COUNTRY%>>%CONFIG_TEMP%
+
+set STATE=MH
+echo What's your state? (MH)
+set /p STATE=
+echo STATE=%STATE%>>%CONFIG_TEMP%
+
+set CITY=Mumbai
+echo What's your city? (Mumbai)
+set /p CITY=
+echo CITY=%CITY%>>%CONFIG_TEMP%
+
+set ORGANIZATION=IT
+echo What's your organization? (IT)
+set /p ORGANIZATION=
+echo ORGANIZATION=%ORGANIZATION%>>%CONFIG_TEMP%
+
+set ORGANIZATION_UNIT=IT Department
+echo What's your organization unit? (IT Department)
+set /p ORGANIZATION_UNIT=
+echo ORGANIZATION_UNIT=%ORGANIZATION_UNIT%>>%CONFIG_TEMP%
+
+set EMAIL=administrator@site.com
+echo What's your email? (administrator@site.com)
+set /p EMAIL=
+echo EMAIL=%EMAIL%>>%CONFIG_TEMP%
+
+echo.
+echo. COUNTRY=%COUNTRY%
+echo. STATE=%STATE%
+echo. CITY=%CITY%
+echo. ORGANIZATION=%ORGANIZATION%
+echo. ORGANIZATION_UNIT=%ORGANIZATION_UNIT%
+echo. EMAIL=%EMAIL%
+
+echo.
+echo Does this looks good to you?
+echo If yes press y to proceed else press n to provide fields again!
+echo.
+
+SET /P CHOICE=Would you like to continue? (y/n) :
+if /i "%CHOICE%"=="n" (
+    cls
+    del /q %CONFIG_TEMP%
+    goto SetConfig
+) else if /i "%CHOICE%"=="y" (
+    goto CopyConfig
+)
+
+:CopyConfig
+
+type %CONFIG_TEMP% > %CONFIG_FILE%
+del /q %CONFIG_TEMP%
+echo.
+echo.Config fields set successfully!
+goto :EOF
+
+:ShowHelp
+
+echo.Usage:
+echo.
+echo.   makecert [options]
+echo.
+echo.Options:
+echo.
+echo.   --no-cfg -n         Do not create the .cfg file.
+echo.
+echo.   --use-config -c     Use the config file to prepopulate secondary fields.
+echo.                       Default: %CONFIG_FILE%
+echo.
+echo.   --set-config -s     Set the secondary fields in config file.
+echo.                       Set to: %CONFIG_FILE%
+echo.
+echo.   --open-repo -o      Open the project repo in the browser.
+echo.                       Repo link: https://github.com/bantya/MakeCert
+echo.
+echo.   --help -h           Show the help dialog.
+echo.
+exit /b
